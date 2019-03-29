5affbae3ab094e39469a25f6c566323b5c249cc1
lovexzq <623324174@qq.com>
2019-03-29 02:42:34 -0400

    完成作業

--- app/public/1.js
+++ app/public/1.js
@@ -12,6 +12,7 @@ document.body.appendChild(script)
 /**
  * 方法2
  */
-// require(['http://x.localhost.com:7001/json?callback=define'], function (value) {
-//   console.log(value)
-// })
+
+require(['http://x.localhost.com:7001/json?callback=define'], function (value) {
+  console.log(value)
+})
--- app/public/3.js
+++ app/public/3.js
@@ -2,7 +2,20 @@ var iframe = document.createElement('iframe')
 iframe.src = 'http://x.localhost.com:7001/public/hash.html'
 document.body.appendChild(iframe)
 
+function getQuery(string){
+    let query = string.replace(/#/,'')
+    query = query.split('&');
+    var obj ={};
+    query.forEach(element => {
+        var temp = element.split('=')
+        obj[temp[0]] = temp[1]
+    });
+    return obj
+}
+
 window.onhashchange = function () {
   // 小练习，做个工具方法，取出query的值
   console.log(location.hash)
+  var query = getQuery(location.hash);
+  console.log(query.msg)
 }
